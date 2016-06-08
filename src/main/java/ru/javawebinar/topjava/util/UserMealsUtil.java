64a7b9a32b1e15981f60465a5e0dd25d894d5858
Makhno Stanislav <mahno.stanislav@mail.ru>
2016-06-08 18:28:44 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -16,38 +16,34 @@ import java.util.*;
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
-//        .toLocalDate();
-//        .toLocalTime();
-        for (UserMealWithExceed list: getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000)
-             ) {
+        getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
+        for (UserMealWithExceed list : getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000)
+                ) {
             System.out.println(list);
         }
     }
 
-    public static List<UserMealWithExceed>  getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        List<UserMealWithExceed> resultList = new ArrayList<>();
-        Map <LocalDate, Integer> calloriesMap = new HashMap<>();
-        for (UserMeal userMeal: mealList) {
-            if (calloriesMap.containsKey(userMeal.getDateTime().toLocalDate())){
-                calloriesMap.replace(userMeal.getDateTime().toLocalDate(),calloriesMap.get(userMeal.getDateTime().toLocalDate()) + userMeal.getCalories());
-            }
-            else {
-                calloriesMap.put(userMeal.getDateTime().toLocalDate(), userMeal.getCalories());
-            }
+    public static List<UserMealWithExceed> getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        Map<LocalDate, Integer> calloriesMap = new HashMap<>();
+        for (int i = 0; i < mealList.size(); i++) {
+            UserMeal userMeal = mealList.get(i);
+            calloriesMap.merge(userMeal.getDateTime().toLocalDate(), userMeal.getCalories(), Integer::sum);
         }
-        for (UserMeal userMeal: mealList
-             ) {
+
+        List<UserMealWithExceed> resultList = new ArrayList<>();
+        for (int i = 0; i < mealList.size(); i++) {
+            UserMeal userMeal = mealList.get(i);
             if (TimeUtil.isBetween(userMeal.getDateTime().toLocalTime(), startTime, endTime)) {
-                resultList.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), calloriesMap.get(userMeal.getDateTime().toLocalDate()) - caloriesPerDay > 0 ));
+                resultList.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), calloriesMap.get(userMeal.getDateTime().toLocalDate()) > caloriesPerDay));
             }
         }
         return resultList;
